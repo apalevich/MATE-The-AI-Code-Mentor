72305924dfbf782d0aad7b31205f7535e83e6b30
apalevich <backstageofme@gmail.com>
2023-08-30 16:25:32 +0400

    Start again from the basics

--- ui/popup.js
+++ ui/popup.js
@@ -1,50 +1,12 @@
-// document.getElementById("start").addEventListener("click", async () => {
-//     const resultDiv = document.getElementById("result");
-//     resultDiv.innerText = "Loading...";
-
-//     const codeContent = await fetchCodeFromGitHub();
-//     if(!codeContent) {
-//         resultDiv.innerText = "Not a valid GitHub code page.";
-//         return;
-//     }
-
-//     try {
-//         const response = await fetch('YOUR_BACKEND_URL', {
-//             method: 'POST',
-//             body: JSON.stringify({ code: codeContent }),
-//             headers: { 'Content-Type': 'application/json' }
-//         });
-        
-//         const data = await response.json();
-//         resultDiv.innerText = data.result;
-//     } catch(error) {
-//         resultDiv.innerText = "Error processing code.";
-//     }
-// });
-
-// async function fetchCodeFromGitHub() {
-//     // Fetch the code content using Chrome Extension's APIs
-//     // If not GitHub or not code, return null or an appropriate message
-// }
-
-// const tabs = await chrome.tabs.query({
-//     url: [
-//       "https://developer.chrome.com/docs/webstore/*",
-//       "https://developer.chrome.com/docs/extensions/*",
-//     ],
-//   });
-
-document.getElementById('startButton').addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      const currentTab = tabs[0];
-      chrome.scripting.executeScript({
-        target: {tabId: currentTab.id},
-        files: ['content.js']
-      }, () => {
-        chrome.tabs.sendMessage(currentTab.id, {action: 'extractContent'}, function(response) {
-          document.getElementById('result').textContent = response || 'No content found.';
-        });
-      });
-    });
-  });
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  const tab = tabs[0];
+  const url = tab.url;
   
+  const messageElement = document.getElementById("message");
+  
+  if (url.includes("github.com")) {
+    messageElement.textContent = "I can check some code here";
+  } else {
+    messageElement.textContent = "Visit Github please";
+  }
+});
