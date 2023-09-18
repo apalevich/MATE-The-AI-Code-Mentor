4ec149d7d8e2c729695ef56355708039d9425311
apalevich <backstageofme@gmail.com>
2023-09-18 17:37:09 +0400

    0.2: set up massaging popup <=>  contentscript

--- ui/content.js
+++ ui/content.js
@@ -7,10 +7,10 @@ function parseTextFromDiv() {
         const text = divElement.textContent;
 
         // Do something with the text, for example, log it to the console
-        console.log("Parsed text:", text);
+        // console.log("Parsed text:", text);
 
         // Return the parsed text if needed
-        return text;
+        return {text, name: 'Developer'};
     } else {
         // Handle the case when the element is not found
         console.error("Element with ID 'read-only-cursor-text-area' not found");
@@ -20,3 +20,28 @@ function parseTextFromDiv() {
 
 // Call the function to parse text from the div
 const parsedText = parseTextFromDiv();
+
+chrome.runtime.onMessage.addListener(
+	function (request, sender, sendResponse) {
+        if (request.action === 'getReview') { 
+            console.log(request)
+            console.log(sender.tab
+                ? `from a content script: ${sender.tab.url}`
+                : `from the extension`);
+            sendResponse({text: `Hello from Content`});
+        }
+        sendResponse({text: "UNKNOWN MESSAGE"})
+	}
+)
+
+// chrome.runtime.onMessage.addListener(
+// 	function (request, sender, sendResponse) {
+//         if (request === 'getReview') {       
+//             console.log(sender.tab
+//                 ? `from a content script: ${sender.tab.url}`
+//                 : `from the extension`);
+//             sendResponse({message: `Hello, ${request.name}`});
+//         }
+//         sendResponse({message: "UNKNOWN MESSAGE"})
+// 	}
+// )
--- ui/popup.js
+++ ui/popup.js
@@ -1,15 +1,17 @@
-document.addEventListener("DOMContentLoaded", checkHealth);
+document.addEventListener("DOMContentLoaded", triggerAction());
 
 async function checkHealth () {
   const responseDiv = document.getElementById("message");
   responseDiv.textContent= 'start';
 
   try {
-    const apiUrl = "https://ai-reviewer-server.onrender.com/health";
+    // const apiUrl = "https://ai-reviewer-server.onrender.com/health";
 
     responseDiv.textContent = 'loading';
 
-    const response = await fetch(apiUrl);
+    // const response = await fetch(apiUrl);
+    const response = await chrome.runtime.sendMessage('getReview');
+	  console.log(response);
 
     if (!response.ok) {
       throw new Error(`Request failed with status: ${response.status}`);
@@ -20,5 +22,41 @@ async function checkHealth () {
   } catch (error) {
     console.error("Error:", error);
     responseDiv.textContent = "Error: " + error.message;
+  }
+};
+
+// async function triggerAction (actionName) {
+// 	// получаем данные вкладки
+// 	const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+// 	// посылаем сообщения во вкладку
+// 	const response = await chrome.tabs.sendMessage(tab.id, {action: actionName});
+// 	console.log(response);
+// };
+
+async function triggerAction () {
+  const tab = await chrome.tabs.query({ active: true, currentWindow: true });
+  const { id, url } = tab[0];
+    
+  console.log('🌎: ', url);
+  try {
+    if (url.includes("github.com")) {
+      const messageElement = document.getElementById("message");
+      const response = await chrome.tabs.sendMessage(id, {action: 'getReview'});
+      
+      console.log('response:', response);
+
+      if (response && response.text) {
+        messageElement.textContent = response.text;
+      } else {
+        messageElement.textContent = "Element not found or empty";
+      };
+    } else {
+      messageElement.textContent = "Visit Github please";
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    // Handle the error as needed, e.g., show an error message in the popup
+    const messageElement = document.getElementById("message");
+    messageElement.textContent = "An error occurred while processing the action.";
   }
 };
