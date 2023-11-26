fefa1487185fed6142e98a734e8dabf8d180d827
apalevich <backstageofme@gmail.com>
2023-11-26 17:06:14 +0400

    Add explicit info on not-supported pages on error

--- ui/content.js
+++ ui/content.js
@@ -2,52 +2,54 @@ console.log("Content script is running");
 
 const parsedText = parseTextFromDiv();
 
-// const domain = "http://localhost:8000/";
 const domain = "https://apalevich.com/backend/";
 
-let cache = chrome.runtime.onMessage.addListener(
-  (req, _sender, sendResponse) => {
-    if (req.action === "getReview") {
-      if (cache) {
-        sendResponse(cache);
-        return true;
-      }
+let cache = null;
 
-      const apiUrl = `${domain}mate/analyze`;
-
-      fetch(apiUrl, {
-        method: "POST",
-        body: JSON.stringify({
-          content: parsedText,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => {
-          if (!response.ok) {
-            alert(`Request failed with status: ${response.status}`);
-            throw new Error(`Request failed with status: ${response.status}`);
-          }
-          return response.json();
-        })
-        .then((responseData) => {
-          const resultsWrapper = { ok: true, responseData };
-          cache = resultsWrapper;
-          sendResponse(resultsWrapper);
-        })
-        .catch((error) =>
-          sendResponse({ ok: false, text: "Error: " + error.message }),
-        );
-
-      return true; // keeps the message channel open until sendResponse is called
-    } else {
-      sendResponse({ ok: false, text: `Error: wrong action "${req.action}"` });
+chrome.runtime.onMessage.addListener((req, _sender, sendResponse) => {
+  if (req.action === "getReview") {
+    if (!parsedText) {
+      sendResponse({ ok: false, text: "Error: No code found on the webpage" });
     }
 
+    if (cache) {
+      sendResponse(cache);
+      return true;
+    }
+
+    const apiUrl = `${domain}mate/analyze`;
+
+    fetch(apiUrl, {
+      method: "POST",
+      body: JSON.stringify({
+        content: parsedText,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Request failed with status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((responseData) => {
+        const resultsWrapper = { ok: true, responseData };
+        cache = resultsWrapper;
+        sendResponse(resultsWrapper);
+      })
+      .catch((error) =>
+        sendResponse({ ok: false, text: "Error: " + error.message }),
+      );
+
     return true; // keeps the message channel open until sendResponse is called
-  },
-);
+  } else {
+    sendResponse({ ok: false, text: `Error: wrong action "${req.action}"` });
+  }
+
+  return true; // keeps the message channel open until sendResponse is called
+});
 
 function parseTextFromDiv() {
   const divElement = document.getElementById("read-only-cursor-text-area");
