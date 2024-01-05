88879d310dc85dbb275f8ca4a834527dcb6727fc
apalevich <backstageofme@gmail.com>
2024-01-05 11:38:38 +0400

    0.6: Made the UI layout

--- src/popup.tsx
+++ src/popup.tsx
@@ -2,17 +2,34 @@ import { useStorage } from "@plasmohq/storage/hook"
 import "~style.css"
 import LoadingAnimation from "~components/loader";
 
+const Footer = () => {
+  return (
+    <div className="tw-flex tw-justify-center tw-py-4">
+      <button onClick={() => {}}>Clear the memory</button>
+    </div>
+  )
+}
+
+const Header = () => {
+  return (
+    <div className="tw-flex tw-justify-center tw-py-4 tw-bg-gradient-to-b tw-from-bg-secondary tw-to-bg-primary">
+      <h1 className="tw-text-xl">
+        MATE
+      </h1>
+    </div>
+  )
+}
+
 const GitHubCodeAnalyzer = () => {
-  const [reviewResult, setReviewResult] = useStorage("review")
+  // const [reviewResult, setReviewResult] = useStorage("review")
 
   return (
-    <div className="plasmo-container">
-      <div className="plasmo-mx-auto">
-        {reviewResult
-        ? JSON.parse(reviewResult.result).feedback
-        : <LoadingAnimation />}
+    <div className="tw-h-screen tw-w-screen tw-flex tw-flex-col tw-justify-between tw-bg-bg-primary">
+      <Header />
+      <div className="tw-mx-auto tw-text-font">
+        <LoadingAnimation />
       </div>
-      <button onClick={() => {setReviewResult(null)}}>Clear the memory</button>
+      <Footer />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{tsx,html}"],
-  darkMode: "media",
-  prefix: "plasmo-"
+  darkMode: "class",  
+  prefix: "tw-",
+  theme: {
+    colors: {
+      'bg-primary': '#F5F5F5',
+      'bg-secondary': '#E0E0E0',
+      'accent': '#4F8BF9',
+      'font': '#333333',
+      'interactive': '#30A7A0',
+      
+      'dark-primary-bg': '#2B2B2B',
+      'dark-secondary-bg': '#3C3F41',
+      'dark-accent': '#FFA726',
+      'dark-text': '#D3D3D3',
+      'dark-interactive': '#00BCD4',
+    },
+    fontFamily: {
+      'mono': ['"Courier New"', 'Courier', 'monospace'],
+    },
+  }
 }
