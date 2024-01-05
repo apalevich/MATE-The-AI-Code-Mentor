9ce3067ea09688e72db3c4e3f6dd10b7bfcc5270
apalevich <backstageofme@gmail.com>
2024-01-05 15:22:03 +0400

    0.6: Add grey colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,18 @@ module.exports = {
       'dark-accent': '#FFA726',
       'dark-text': '#D3D3D3',
       'dark-interactive': '#00BCD4',
+      grey: {
+        "050": "#F7F7F7",
+        "100": "#E1E1E1",
+        "200": "#CFCFCF",
+        "300": "#B1B1B1",
+        "400": "#9E9E9E",
+        "500": "#7E7E7E",
+        "600": "#626262",
+        "700": "#515151",
+        "800": "#3B3B3B",
+        "900": "#222222",
+      }
     },
     fontFamily: {
       'mono': ['"Courier New"', 'Courier', 'monospace'],
