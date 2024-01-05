a9ca2ee5076c832181c448f5713631b5e00339d0
apalevich <backstageofme@gmail.com>
2024-01-05 15:07:20 +0400

    0.6:  Apply folders structure to components

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <div className="tw-flex tw-justify-center tw-py-4">
+        <button onClick={() => {}}>Clear the memory</button>
+      </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/header/header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+      <div className="tw-flex tw-justify-center tw-py-4 tw-bg-gradient-to-b tw-from-bg-secondary tw-to-bg-primary">
+        <h1 className="tw-text-xl">
+          MATE
+        </h1>
+      </div>
+    )
+}
+
+export default Header;
