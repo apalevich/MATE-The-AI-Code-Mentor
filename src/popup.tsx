fac024cf36e3b01ece37b2912c309cb9be5a6705
apalevich <backstageofme@gmail.com>
2024-01-06 10:08:30 +0400

    0.6: Complete render for results

--- src/popup.tsx
+++ src/popup.tsx
@@ -5,14 +5,40 @@ import Header from "~components/header";
 import Footer from "~components/footer";
 import ResultView from "~components/resultView";
 
+const mockObj = {
+  overallScore: '66',
+  shortestFeedback: 'Good!',
+  overallFeedback: 'Your code contains types and abstracted enough',
+  mustHaveRecommendations: [
+    {
+      title: 'Real Time Location',
+      description: 'Asperiores nemo possimus nesciunt quam mollitia.'
+    },
+    {
+      title: 'Real Time Location',
+      description: 'Asperiores nemo possimus nesciunt quam mollitia.'
+    },
+  ],
+  niceToHaveRecommendations: [
+    {
+      title: 'Real Time Location',
+      description: 'Asperiores nemo possimus nesciunt quam mollitia.'
+    },
+    {
+      title: 'Real Time Location',
+      description: 'Asperiores nemo possimus nesciunt quam mollitia.'
+    },
+  ],
+}
+
 const GitHubCodeAnalyzer = () => {
   // const [reviewResult, setReviewResult] = useStorage("review")
 
   return (
-    <div className="tw-h-screen tw-w-screen tw-flex tw-flex-col tw-justify-between tw-bg-bg-primary">
+    <div className="tw-h-100 tw-w-screen tw-flex tw-flex-col tw-justify-between tw-bg-bg-primary">
       <Header />
       <div className="tw-mx-auto tw-text-font">
-        <ResultView />
+        <ResultView {...mockObj} />
       </div>
       <Footer />
     </div>
