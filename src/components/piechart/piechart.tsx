f330a2fb3afadb3ae13cb99e9d2e6e5fbb67fc13
apalevich <backstageofme@gmail.com>
2024-01-08 23:32:39 +0400

    0.6: Add piechart

--- src/components/piechart/piechart.tsx
+++ src/components/piechart/piechart.tsx
@@ -1,9 +1,22 @@
-// https://codepen.io/team/css-tricks/pen/gaLKga/77040610b66e6aa0d0fb87022c524194
+import './piechart.css';
+import { useEffect, useRef } from 'react';
 
-const PieChart = ({value}) => {
+const PieChart = ({ value }) => {
+    const total = 158; /* 2π × 25 */
+    const fillValue = ((value * total) / 100);
+    const pieRef = useRef(null);
+
+    useEffect(() => {
+        if (pieRef.current) {
+            pieRef.current.style.strokeDasharray = `${fillValue} ${total}`;
+        }
+    }, [fillValue]);
+
     return (
-        <div className="tw-text-5xl">{value}%</div>
-    )
+        <svg width="100" height="100" className="chart">
+            <circle r="25" cx="50" cy="50" className="pie" ref={pieRef} />
+        </svg>
+    );
 }
 
 export default PieChart;
