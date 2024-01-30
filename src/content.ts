dc8ca9ec4d16f35e5b8d9eec7e8a3e9f125b6f9e
apalevich <backstageofme@gmail.com>
2024-01-30 19:20:50 +0400

    0.7: Format code

--- src/content.ts
+++ src/content.ts
@@ -1,19 +1,22 @@
-import type { PlasmoCSConfig } from "plasmo"
-import { sendToBackground } from "@plasmohq/messaging"
+import type { PlasmoCSConfig } from "plasmo";
+import { sendToBackground } from "@plasmohq/messaging";
 
 export const config: PlasmoCSConfig = {
   matches: ["<all_urls>"],
   all_frames: true,
-}
+};
 
 window.addEventListener("load", () => {
   const parsedText = document.getElementById("read-only-cursor-text-area")?.textContent;
 
-  sendToBackground({
-    name: "review",
-    body: {
-      content: parsedText
-    },
-    extensionId: chrome.runtime.id
-  })
+  if (parsedText) {
+    sendToBackground({
+      name: "review",
+      body: {
+        content: parsedText
+      },
+      extensionId: chrome.runtime.id
+    });
+  }
+  return false
 })
