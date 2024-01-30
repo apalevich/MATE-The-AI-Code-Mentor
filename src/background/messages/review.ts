147a92229325948f1b7a6a80277524bbb433f089
apalevich <backstageofme@gmail.com>
2024-01-30 19:22:41 +0400

    0.7: Implement caching mechanism

--- src/background/messages/review.ts
+++ src/background/messages/review.ts
@@ -1,13 +1,55 @@
 import type { PlasmoMessaging } from "@plasmohq/messaging"
 import { Storage } from "@plasmohq/storage"
 import MateService from "src/mate-service";
+import md5 from "blueimp-md5";
 
-const storage = new Storage()
+const storage = new Storage();
 const service = new MateService();
+storage.removeAll()
+
+type reviewType = {
+  id: string,
+  result: object,
+};
  
+const getCachedReview = async (previousReviews, id) => {
+  console.log(previousReviews);
+  const cachedReview = previousReviews.find(r => r.id === id);
+  return cachedReview;
+}
+
 const handler: PlasmoMessaging.MessageHandler = async (req, _res) => {
-  const review = await service.getReview(req.body.content);
-  storage.set("review", review)
+  const sourceCode = req.body.content;
+  const hash = md5(sourceCode);
+
+  // if (!previousReviews) {
+  //   await storage.set('previousReviews', [])
+  // }
+
+  const previousReviews: reviewType[] = await storage.get('previousReviews') || [];
+  const previousReview = await getCachedReview(previousReviews, hash);
+
+  let review = await getCachedReview(previousReviews, hash);
+  if (review) {
+    console.log('Found cached review: ', review)
+  } 
+  if (!review) {
+    console.log('Cached review not found, making request')
+    const generatedReview = await service.getReview(req.body.content);
+    const { result } = generatedReview;
+    if (generatedReview.ok){
+      review = {
+        id: hash,
+        result: JSON.parse(result)
+      };
+      
+      await storage.set('previousReviews', [...previousReviews, review])
+    } else {
+      console.log("Backend request failed: ", generatedReview);
+    }
+  };
+
+  await storage.set('currentReview', review);
 }
  
 export default handler
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,22 +1,22 @@
-import { useStorage } from "@plasmohq/storage/hook"
-import "~style.css"
+import { useStorage } from "@plasmohq/storage/hook";
+import "~style.css";
 import LoadingAnimation from "~components/loader";
 import Header from "~components/header";
 import Footer from "~components/footer";
 import ResultView from "~components/resultView";
 
 const GitHubCodeAnalyzer = () => {
-  const [reviewResult, setReviewResult] = useStorage("review")
+  const [currentReview] = useStorage("currentReview");
 
   return (
     <div className="tw-h-100 tw-w-screen tw-flex tw-flex-col tw-justify-between tw-bg-bg-primary">
       <Header />
       <div className="tw-mx-auto tw-text-font">
-        {reviewResult === undefined ? (
+        {currentReview === undefined ? (
           <LoadingAnimation />
-        ) : reviewResult.ok ? (
-          <ResultView {...JSON.parse(reviewResult.result)} />
-        ) : "Error"}
+        ) : 
+          <ResultView {...currentReview.result} />
+        }
       </div>
       <Footer />
     </div>
