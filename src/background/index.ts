875f5b6b69f7a1248f29f0131ceb387da9c492bf
apalevich <backstageofme@gmail.com>
2024-04-18 19:59:00 +0400

    0.9: Remove redundant code

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,9 +1,7 @@
-chrome.sidePanel
-  .setPanelBehavior({ openPanelOnActionClick: true })
-  .catch((error) => console.error(error));
+chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: false })
 
 chrome.tabs.onUpdated.addListener(async (tabId, _, tab) => {
-    if (tab.url.startsWith('https://github.com') && tab.url.includes('blob')) {
+    if (tab.url?.startsWith('https://github.com') && tab.url.includes('blob')) {
         await chrome.sidePanel.setOptions({
             tabId,
             enabled: true
