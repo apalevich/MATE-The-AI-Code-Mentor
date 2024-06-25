6e73e3fad100c71cb8eecfcb34726ae947fa8b7b
apalevich <backstageofme@gmail.com>
2024-06-25 17:59:28 +0400

    v0.1: Apply MATE

--- src/options.tsx
+++ src/options.tsx
@@ -3,28 +3,27 @@ import { useEffect } from "react"
 import { useStorage } from "@plasmohq/storage/hook"
 import { supabase } from "~core/supabase"
 import '~style.css';
- 
+
 function IndexOptions() {
   const [user, setUser] = useStorage<User>("user")
- 
+
   useEffect(() => {
-    initSession()
+    initializeSession()
   }, []);
- 
-  const initSession = async () => {
+
+  const initializeSession = async () => {
     const { data, error } = await supabase.auth.getSession()
- 
     if (error) {
       console.error(error)
       return
     }
-    if (!!data.session) {
+    if (data.session) {
       console.log('Supabase data: ', data);
       setUser(data.session.user)
     }
   }
 
-  const handleOAuthLogin = async (provider: Provider, scopes = "email") => {
+  const signInWithOAuth = async (provider: Provider, scopes = "email") => {
     await supabase.auth.signInWithOAuth({
       provider,
       options: {
@@ -34,17 +33,17 @@ function IndexOptions() {
     })
   }
 
-  const handleSignOut = async () => {
+  const signOut = async () => {
     await supabase.auth.signOut()
     setUser(null)
   }
 
   const renderSignIn = () => (
     <>
-      <h2 className="tw-pl-0 tw-pb-1 tw-border-b-2 tw-border-blue-600 tw-text-2xl tw-uppercase tw-inline-block tw-font-thin"> sign in </h2>
+      <h2 className="tw-pl-0 tw-pb-1 tw-border-b-2 tw-border-blue-600 tw-text-2xl tw-uppercase tw-inline-block tw-font-thin">Sign In</h2>
       <button
         className="tw-bg-blue-600 tw-text-white tw-w-full tw-py-2 tw-px-5 tw-block tw-h-10 tw-rounded-full tw-mt-8 tw-transition-all tw-duration-500 tw-ease-in-out tw-border-none tw-uppercase hover:tw-bg-blue-700 hover:tw-shadow-lg hover:tw-cursor-pointer focus:tw-outline-none"
-        onClick={() => handleOAuthLogin('github')}
+        onClick={() => signInWithOAuth('github')}
       >
         Sign In with GitHub
       </button>
@@ -65,7 +64,7 @@ function IndexOptions() {
       </button>
       <button
         className="tw-bg-white tw-text-blue-600 tw-border-2 tw-border-blue-600 tw-w-full tw-py-2 tw-px-5 tw-block tw-h-10 tw-rounded-full tw-mt-8 tw-transition-all tw-duration-500 tw-ease-in-out tw-border-none tw-uppercase hover:tw-bg-blue-700 hover:tw-shadow-lg hover:tw-cursor-pointer focus:tw-outline-none"
-        onClick={handleSignOut}
+        onClick={signOut}
       >
         Want to logout?
       </button>
@@ -80,5 +79,5 @@ function IndexOptions() {
     </main>
   )
 }
- 
+
 export default IndexOptions
