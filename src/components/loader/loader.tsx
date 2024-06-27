acc904a0a3f6f139a355bd5861f50dfb27c09ff4
apalevich <backstageofme@gmail.com>
2024-06-27 12:11:33 +0400

    v0.1: Fix indentation

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -5,13 +5,17 @@ const LoadingAnimation = ({sideSize = 10}) => {
 
     return (
         <div className=" tw-my-16">
-            {Array.from(opt, (_, rowIndex) => (
-            <div className="row tw-flex tw-flex-grow-0 tw-flex-shrink-1 tw-w-full" key={rowIndex}>
-                {Array.from(opt, (_, circleIndex) => (
-                <div className="circle tw-bg-accent tw-w-3 tw-h-3 tw-m-[10] tw-rounded-full" key={circleIndex}></div>
-                ))}
-            </div>
-            ))}
+            {
+            Array.from(opt, (_, rowIndex) => (
+                <div className="row tw-flex tw-flex-grow-0 tw-flex-shrink-1 tw-w-full" key={rowIndex}>
+                    {
+                        Array.from(opt, (_, circleIndex) => (
+                            <div className="circle tw-bg-accent tw-w-3 tw-h-3 tw-m-[10] tw-rounded-full" key={circleIndex}></div>
+                        ))
+                    }
+                </div>
+            ))
+            }
         </div>
     );
 };
